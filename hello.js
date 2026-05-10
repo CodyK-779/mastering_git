c1f30102fa68b5d87ec870e7c6e684f83f2f858f
CodyK-779 <Codyk9627@Gmail.com>
2026-05-10 15:49:33 +0630

    Trying out revert

--- hello.js
+++ hello.js
@@ -2,3 +2,4 @@ console.log("Hello, Git!");
 console.log("Hello, GitHub!");
 console.log("Hello from main branch");
 console.log("This is the fourth line in hello.js");
+console.log("TRYING OUT REVERT!")
